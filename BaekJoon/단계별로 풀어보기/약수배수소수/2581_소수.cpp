9fe767ac949544706257b07e97e522057a223a12
onasheep <inabass@naver.com>
2023-04-13 23:02:47 +0900

    Feat : add BaekJoon algorithm factor, multiple, decimal - 4

---
+++ BaekJoon/단계별로 풀어보기/약수배수소수/2581_소수.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num)
+{
+	if (num <= 1)
+	{
+		return false;
+	}
+	int sqrtNum = sqrt(num);
+	for (int i = 2; i <= sqrtNum; i++)
+	{
+		if (num % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int m, n;
+	int sum = 0;
+	int count = 0;
+	int smallPrime = 0;
+	bool smallPrimeCheck = false;
+	cin >> m >> n;
+
+	for (int i = m; i <= n; i++)
+	{
+
+		if (isPrime(i))
+		{
+			if (!smallPrimeCheck)
+			{
+				smallPrime = i;
+				smallPrimeCheck = true;
+			}
+			sum += i;
+		}
+		else
+		{
+			count++;
+		}
+	}
+	if (count == n - m + 1)
+	{
+		cout << -1;
+	}
+	else
+	{
+		cout << sum << "\n" << smallPrime;
+	}
+	return 0;
+}
