eaf45611dd29833b50140cb50e0d2e3ed9dc6dd4
onasheep <inabass@naver.com>
2023-04-23 17:48:53 +0900

    Feat : add BaekJoon algorithm time complexity - 1

---
+++ BaekJoon/단계별로 풀어보기/시간복잡도/24262_알고리즘수행시간1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int n;
+
+	cin >> n;
+
+	cout << 1 << "\n" << 0;
+	return 0;
+}
+
---
+++ BaekJoon/단계별로 풀어보기/시간복잡도/24263_알고리즘수행시간2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int n;
+
+	cin >> n;
+
+	cout << n << "\n" << 1;
+	return 0;
+}
