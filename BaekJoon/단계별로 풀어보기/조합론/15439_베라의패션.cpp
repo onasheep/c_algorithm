d368d8121dde35f94ad025e7573c5aedf0adb8b1
onasheep <inabass@naver.com>
2023-06-01 03:32:24 +0900

    Feat : add BaekJoon algorithm combinatorics - 1

---
+++ BaekJoon/단계별로 풀어보기/조합론/15439_베라의패션.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	int n;
+	cin >> n;
+
+
+	cout << 2 * n;
+	return 0;
+}
+
+
