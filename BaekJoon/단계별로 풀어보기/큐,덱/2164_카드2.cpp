9493d414988d20be210b6279e425f73e1c176c55
onasheep <inabass@naver.com>
2023-06-06 00:17:01 +0900

    Feat : add BaekJoon algorithm Queue,Deck - 1

---
+++ BaekJoon/단계별로 풀어보기/큐,덱/2164_카드2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	queue<int> q;
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+
+	while (q.size() != 1)
+	{
+		q.pop();
+
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.back();
+
+	return 0;
+}
